91fa5f16b00c61b04e08b26613f43879dd97f774
Ini-Abasi Bernard <55382743+Inyrkz@users.noreply.github.com>
2020-05-25 11:15:24 +0100

    add postgres uri

--- settings.py
+++ settings.py
@@ -5,5 +5,5 @@ from flask import Flask
 app = Flask(__name__)
 
 # configuring the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://urmvplnlinfrnw:f555a59e48eb360d84a441de9e21ed9714b48d8b68126369eaa660a802da7656@ec2-52-202-22-140.compute-1.amazonaws.com:5432/d9bke6t472udgq'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
